f2c27f2ceed88da930e42cee12ca059c6951ad6d
solom <s201172@student.dtu.dk>
2023-12-10 12:31:29 +0100

    almoust done

---
+++ Day5/password_generator.py
@@ -0,0 +1,33 @@
+import random
+
+
+letters = ['a','b','c','d','e','f','g','h']
+numbers = ['1','2','3','4','5','6','7','8','9']
+symbols = ['!','#','%','&','(',')','+','*']
+
+print("Welcome to the password generator")
+
+nr_letter = int(input("choose how many leters \n"))
+nr_numbers = int(input("choose how many numbers \n"))
+nr_symbols = int(input("choose how many symbols \n"))
+
+
+
+# password = ""
+
+# for letter in range(1, nr_letter+1):
+    
+
+
+m = (random.sample(letters, nr_letter)) 
+n = (random.sample(numbers, nr_numbers))
+b = (random.sample(symbols, nr_symbols)) 
+print(m)
+print(n)
+print(b)
+simple_code = m + n + b
+print(simple_code)
+print(*simple_code)
+random.shuffle(simple_code) 
+hard_code = simple_code
+print(*hard_code)
