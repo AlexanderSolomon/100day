2557e5a95a337a5ac52905ce6fa62a91d8a2f73e
alexsolomon <alexsolomon91@gmail.com>
2024-08-23 15:27:20 +0200

    rearrange

--- Day15/coffeMachine/main.py
+++ Day15/coffeMachine/main.py
@@ -52,29 +52,8 @@ def report():
     print(f"Coffe: {resources['coffee']}.g  \nMilk: {resources['milk']}.ml \nWater: {resources['water']}.ml \nMoney: {resources['money']}$")
 
 
-# TODO Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-"""
-a. Check the user’s input to decide what to do next.
-b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
-"""
-# while machine == True:
-#     coffeChoise = input("would you like: 1. Espresso, 2.Latte or 3.Cappucino \n")
-#     if coffeChoise == "1":
-#         print("you choose espresso")
-#     elif coffeChoise == "2":
-#         print("you choose latte")
-#     elif coffeChoise == "3":
-#         print("you choose cappucino")
-#     elif coffeChoise == "off":
-#         machine = False
-#     elif coffeChoise == "report":
-#         report()
 
-# TODO Turn off the Coffee Machine by entering “off” to the prompt.  DONE
-"""
-a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine. 
-Your code should end execution when this happens
-"""
+
 
 # TODO Check resources sufficient?
 """
@@ -82,7 +61,6 @@ a. When the user chooses a drink, the program should check if there are enough r
 b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue to make the drink but print: “Sorry there is not enough water.”
 c. The same should happen if another resource is depleted, e.g. milk or coffee."""
 
-#print(resources["coffee"] -20)
 
 #espresso 
 def espresso():
@@ -96,12 +74,41 @@ def latte():
     resources["coffee"] -= 18
     resources["money"] += 2.5
 
+
+
+
+# TODO Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+"""
+a. Check the user’s input to decide what to do next.
+b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
+"""
+while machine == True:
+    coffeChoise = input("would you like: 1. Espresso, 2.Latte or 3.Cappucino \n")
+    if coffeChoise == "1":
+        print("you choose espresso")
+        espresso()
+    elif coffeChoise == "2":
+        print("you choose latte")
+    elif coffeChoise == "3":
+        print("you choose cappucino")
+    elif coffeChoise == "off":
+        machine = False
+    elif coffeChoise == "report":
+        report()
+
+# TODO Turn off the Coffee Machine by entering “off” to the prompt.  DONE
+"""
+a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine. 
+Your code should end execution when this happens
+"""
+
+
 #testing the subtractions
-print(resources)
-espresso()
-print(resources)
-latte()
-print(resources)
+# print(resources)
+# espresso()
+# print(resources)
+# latte()
+# print(resources)
 
 #latte
 
