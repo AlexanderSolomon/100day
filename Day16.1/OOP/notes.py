a698e26b2aaa5223eaf2c265c704982a49bd0e4d
alexsolomon <alexsolomon91@gmail.com>
2024-08-24 23:37:57 +0200

    getting turtle to work in vsc

---
+++ Day16.1/OOP/notes.py
@@ -0,0 +1,26 @@
+"""Oop
+
+## Waiter 
+
+ATTRIBUTES = VARIABLES
+ATTRIBUTES / Has :
+is_holding_plate = TRUE
+Tables_responsible_for[3,4,5]
+
+Its a function = things it can do
+METHODS / Does:
+Def takes_order(table, order):
+	#takes order to chef
+Def takes_payments(amount):
+	#add money to the restaurant"""
+ 
+ 
+ # def function() 
+ #car.function()
+ 
+ #object.attribute
+ 
+#import turtle
+#timmy = turtle.Turtle()
+
+####################################
---
+++ Day16.1/OOP/tst.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+import turtle
+  
+
+ws = turtle.Screen()
+
+ws.bgcolor("cyan")
+turtle.done()
+
+
